d8059c85bc79a2658851bddb3d43c9a1d8b82ca2
Aleksandr-Glebik <glebikaa@mail.ru>
2021-11-25 23:24:22 +0300

    feat: add method post, post obj from form

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
 //движок который рендерит стр написан на ejs - при запросе на корнеую стр нашего приложения (методом get) мы рендерим файл index с расширением ejs
 
 app.get('/', (req, res) => {
     // res.end('Hello from nodejs')
+    res.render('index')
+})
+
+app.post('/', (req, res) => {
+    // console.log(req.body);
+    const { city } = req.body
+    console.log(city);
     res.render('index')
 })
 
