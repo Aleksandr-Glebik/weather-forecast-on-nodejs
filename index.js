1a38f23b2207373c4162a17bc4cfb6cb350cdcc6
Aleksandr-Glebik <glebikaa@mail.ru>
2021-11-25 23:01:00 +0300

    feat: index.ejs and markup

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+//движок который рендерит стр написан на ejs - при запросе на корнеую стр нашего приложения (методом get) мы рендерим файл index с расширением ejs
 app.get('/', (req, res) => {
-    res.end('Hello from nodejs')
+    // res.end('Hello from nodejs')
+    res.render('index')
 })
 
 app.listen(3000, () => {
